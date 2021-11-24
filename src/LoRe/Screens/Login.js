ef000fc0e1749386ccd62554e6a442adec713bc1
fabian-ss <randommusicd@gmail.com>
2021-11-23 23:20:57 -0600

    Base de formulario sin configurar, utilizando Formik

--- src/LoRe/Screens/Login.js
+++ src/LoRe/Screens/Login.js
@@ -9,11 +9,36 @@ import { Octicons, Ionicons, Fontisto } from '@expo/vector-icons';
 // API
 import axios from 'axios';
 
-import { Colors } from './../styles/styles'
-const { primary,secondary, tertiary, quaternary } = Colors;
+// Colores 
+import { Colors,stylesLogin,PageLogo } from './../styles/styles';
+const { primary,secondary, tertiary, quaternary,brand } = Colors;
 
 // Redux
 import { useDispatch } from 'react-redux';
+
+// import { Form, Control } from 'react-redux-form/native';
+
+import { Formik } from 'formik';
+
+import{
+
+  StyledFormArea, 
+  MsgBox,
+  StyledButton,
+  ButtonText,
+  Line,
+  ExtraView,
+  ExtraText,
+  TextLink,
+  TextLinkContent,
+  LeftIcon,
+  StyledTextInput,
+  StyledInputLabel,
+  RigthtIcon,
+
+
+  
+} from './../styles/styles';
 
 const Login = ({navigation}) => {
 
@@ -21,20 +46,106 @@ const Login = ({navigation}) => {
   const height = Dimensions.get('window').height;
   console.log(width,height, width/height);
 
+  const [hidePassword, setHidePassword] = useState(true);
+  const [message,setMessage] = useState('');
+  const [messageType,setMessageType] = useState('');
+
   return (
- <SafeAreaView style={styles.container}>
+ <SafeAreaView style={stylesLogin.container}>
       <StatusBar 
       style="auto" 
       animated={true}
-      backgroundColor="#61dafb"
+      backgroundColor="#091115"
       />
 
-    <View style={styles.headerlogin} >
-      <Text>Header bolis</Text>
+    <View style={stylesLogin.headerlogin} >
+        <View style={stylesLogin.containhead}>
+          <Text style={stylesLogin.containheadtext} >RandoCode Lab</Text>
+          {/* <PageLogo resizeMode="cover" source={require('./../assets/img/conejito.png')}/> */}
+
+        </View>
     </View>
 
-    <View style={styles.bodylogin} >
-      <Text>App cuerpo</Text>
+    <View style={stylesLogin.bodylogin} >
+
+        <View style={stylesLogin.bodyloginsuper}>
+          <PageLogo resizeMode="cover" source={require('./../../../assets/img/conejito.png')}/>
+
+        </View>  
+
+        <View style={stylesLogin.bodylogininfer}>
+          <View style={stylesLogin.bodylogininferTitle}>
+            <Text style={stylesLogin.containheadtext}>Inicio de Sesión</Text>
+          </View>   
+          <View style={stylesLogin.bodylogininferfields}>
+            <Text style={stylesLogin.containheadtext}>Campos a rellenar</Text>
+            <Formik
+            initialValues={{ username: '', password: '' }}
+            onSubmit={(values, {setSubmitting}) => {
+                if (values.username == '' || values.password == ''){
+                    handleMessage("Todos los campos son obligatorios");
+                    setSubmitting(false);
+                } else {
+                    handleLogin(values,setSubmitting);
+                };
+
+                console.log(values);
+                // navigation.navigate('Welcome');
+            }}
+            >
+                {({ handleChange, handleBlur,handleSubmit,values,isSubmitting }) => (
+                    <StyledFormArea>
+                    <MyTextInput
+                        label="Ingresa nombre de usuario"
+                        icon="mail"
+                        placeholder="username"
+                        placeholderTextColor={'#9CA3AF'}
+                        onChangeText={handleChange('username')}
+                        onBlur={handleBlur('username')}
+                        value={values.username}
+                        // keyboardType="username-address"
+                    />
+                    <MyTextInput
+                        label="Ingresa tu contraseña"
+                        icon="lock"
+                        placeholder="* * * * * * * * * * "
+                        placeholderTextColor={'#9CA3AF'}
+                        onChangeText={handleChange('password')}
+                        onBlur={handleBlur('password')}
+                        value={values.password}
+                        secureTextEntry={hidePassword}
+                        isPassword={true}
+                        hidePassword={hidePassword}
+                        setHidePassword={setHidePassword}
+                    />    
+                    <MsgBox type ={messageType}>{message}</MsgBox>
+                    {!isSubmitting &&
+                    <StyledButton onPress={handleSubmit}>
+                        <ButtonText>Ingresar</ButtonText>
+                    </StyledButton> }     
+
+                    {isSubmitting &&
+                    <StyledButton disabled={true} >
+                        <ActivityIndicator size="large" color={primary}/>
+                    </StyledButton> }                        
+
+                    <Line />
+                    <StyledButton google={true} onPress={handleSubmit}>
+                        <Fontisto name="google" size={25} color='rgb(0, 0, 26)'/>
+                        <ButtonText google={true}>Sign in with Google</ButtonText>
+                    </StyledButton>
+                    <ExtraView>
+                        <ExtraText>¿Aún no tienes una cuenta?</ExtraText>    
+                        <TextLink onPress={()=>{navigation.navigate('Signup')}}>
+                        <TextLinkContent>Signup</TextLinkContent>
+                        </TextLink>
+                    </ExtraView>   
+                    </StyledFormArea>
+                )}
+            </Formik>
+          </View>   
+        </View>    
+
     </View>
   
 
@@ -42,33 +153,21 @@ const Login = ({navigation}) => {
   )
 }
 
+const MyTextInput = ({ label, icon, isPassword, hidePassword, setHidePassword, ...props}) => {
+  return (
+      <View>
+      <LeftIcon>
+          <Octicons name={icon} size={30} color={brand}/>
+      </LeftIcon>
+      <StyledInputLabel>{label}</StyledInputLabel>
+      <StyledTextInput {...props} />
+      {isPassword && (
+          <RigthtIcon onPress={()=> setHidePassword(!hidePassword)} >
+              <Ionicons name={ hidePassword ? 'md-eye-off' : 'md-eye'} size={30} color='rgb(0, 0, 26)'/>
+          </RigthtIcon>)}
+      </View>
+  );
+};
+
+
 export default Login;
-
-const styles = StyleSheet.create({
-
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    // paddingTop:Constants.statusBarHeight, 
-
-  },
-
-    headerlogin: {
-      backgroundColor: 'red',
-      width: Dimensions.get('window').width,
-      height: Dimensions.get('window').height*(1/16),
-
-    },  
-
-    bodylogin: {
-      backgroundColor: 'green',
-      width: Dimensions.get('window').width,
-      height: Dimensions.get('window').height*(15/16),
-
-    },  
-
-});
-
--- src/LoRe/styles/styles.js
+++ src/LoRe/styles/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import Constants from 'expo-constants';
+import { View,StyleSheet,Text, Dimensions,SafeAreaView,StatusBar,Image } from 'react-native';
 
 const StatusBarHeight = Constants.statusBarHeight;
 
@@ -9,4 +10,190 @@ export const Colors = {
  secondary : "rgb(0, 41, 0)",
  tertiary : "rgb(0, 0, 26)",
  quaternary : "rgb(116, 41, 26)",
+ brand: "rgb(116, 0, 0)",
+ darkLight: "rgb(0, 0, 26)",
 };
+
+export const stylesLogin = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#091115',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        // paddingTop:Constants.statusBarHeight, 
+
+      },    
+
+        headerlogin: {
+            backgroundColor: '#091115',
+            width: Dimensions.get('window').width,
+            height: Dimensions.get('window').height*(1/16),
+            justifyContent: 'center',
+            alignItems: 'center',
+            
+        },    
+
+            containhead: {
+                backgroundColor: '#091115',
+                width: Dimensions.get('window').width*(15.7/16),
+                height: Dimensions.get('window').height*(.8/16),
+                justifyContent: 'center',
+                alignItems: 'center',
+
+            },
+            
+                containheadtext: {
+                    color: '#fff',
+
+                },
+
+        bodylogin: {
+            backgroundColor: 'cyan',
+            width: '100%',
+            height:'100%',
+        },
+
+            bodyloginsuper: {
+                // backgroundColor: 'blue',
+                width: '100%',
+                height:'18%',
+                minHeight: 140,
+                maxHeight: 150,
+                justifyContent: 'center',
+                alignItems: 'center',
+
+            },  
+            bodylogininfer: {
+                backgroundColor: 'red',
+                color: 'white',
+                width: '100%',
+                height:'80%',
+
+                },  
+                bodylogininferTitle: {
+                    backgroundColor: 'yellow',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height:'10%',
+                    },  
+                bodylogininferfields: {
+                    backgroundColor: 'rgb(255,200,100)',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height:'90%',
+                },  
+
+});
+
+export const PageLogo = styled.Image`
+    width: 125px;
+    height: 125px;
+    /* width: 125px;
+    height: 125px; */
+`;
+
+export const StyledFormArea = styled.View`
+    width: 90%;
+`;
+
+export const MsgBox = styled.Text`
+    text-align: center;
+    font-size: 13px;
+`;
+
+
+export const StyledButton = styled.TouchableOpacity`
+    padding: 15px;
+    background-color: 'rgb(0, 0, 26)';
+    justify-content: center;
+    align-items: center;
+    border-radius: 5px;
+    height: 55px;
+    /* marginVertial:5px; */
+
+    ${props => props.google == true && `
+        background-color: rgb(0, 0, 26);
+        flex-direction: row;
+        justify-content: center;
+    `}
+`;
+
+
+export const ButtonText = styled.Text`
+    color: rgb(0, 0, 26);
+    font-size: 16px;
+
+    ${(props) => props.google == true && `
+        padding-left: 10px;
+        padding-right: 10px;
+        justify-content: center;
+    `}
+
+`;
+
+export const Line = styled.View`
+    width: 100%;
+    height: 1px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    background-color: 'rgb(0, 0, 26)';    
+`; 
+
+export const ExtraView = styled.View`
+    justify-content: center;
+    flex-direction: row;
+    align-items: center;
+    padding: 10px;
+`;
+
+export const ExtraText = styled.Text`
+    justify-content: center;
+    align-items: center;
+    color: 'rgb(0, 0, 26)';
+    font-size: 15px;
+`;
+
+export const TextLink = styled.TouchableOpacity`
+    justify-content: center;
+    align-items: center;
+    margin-left: 7px;
+`;
+
+export const TextLinkContent = styled.Text`
+    color: 'rgb(0, 0, 26)';
+    font-size: 15px;
+`;
+
+export const LeftIcon = styled.View`
+    left: 15px;
+    top: 34px;
+    position: absolute;
+    z-index: 1;
+`;
+
+export const StyledTextInput = styled.TextInput`
+    background-color: 'rgb(0, 0, 26)';
+    padding: 15px;
+    padding-left: 55px;
+    border-radius: 5px;
+    font-size: 16px;
+    height: 60px;
+    margin-bottom: 10px;
+    color: 'rgb(0, 0, 26)';
+    /* marginVertial:3px; */
+`;
+
+export const StyledInputLabel = styled.Text`
+    color: 'rgb(0, 0, 26)';
+    font-size: 13px;
+    text-align: left;
+`;
+
+export const RigthtIcon = styled.TouchableOpacity`
+    right: 15px;
+    top: 34px;
+    position: absolute;
+    z-index: 1;
+`;
